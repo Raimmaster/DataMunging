396e20144745e9030bffb3acf54f8211c06ca4dd
Raim <rai.interiano@gmail.com>
2016-11-10 19:53:01 -0600

    Makes correct splits and separation of columns for Football file

--- src/FootballData.java
+++ src/FootballData.java
@@ -1,35 +1,38 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
 
 public class FootballData {
-
+    
+    public static String leaveOneWhiteSpace(String line){
+        String newLine = line.replaceAll("\\s+", " ");
+        newLine = newLine.replace(" -", "");
+        
+        return newLine;
+    }
+    
     public static int findSmallestGoalDifference() throws IOException {
         FileReader file = new FileReader("football.dat");
         
         BufferedReader buffer = new BufferedReader(file);
         String fileLine = null;
-        
+        ArrayList<String[]> footballColumns = new ArrayList();
+        //final int amountOfColumns = 9;
         while((fileLine = buffer.readLine()) != null){
             if(Character.isDigit(fileLine.trim().charAt(0))){
-                System.out.println(fileLine.trim());
-                
-                //int indexOfColumn = 0;
-                int footballColumns[] = new int[8];
-                
-                int indexToCheck = 0;
-                int indexOfFootballColumn = 0;
-                char charToCompare;
-                
-                while(fileLine.charAt(indexToCheck++) != ' '){
-                    while(fileLine.charAt(indexToCheck) == ' ');
-                    footballColumns[indexOfFootballColumn] = indexToCheck;
-                }
-                
-                for(int i : footballColumns)
-                    System.out.println("Index: " + i);
-                //System.out.printf("Index of next column: %d \n", indexOfColumn);
+                String newFileLine = leaveOneWhiteSpace(fileLine.trim());
+                System.out.println(newFileLine);
+                String [] footballLine = newFileLine.split(" ");
+                footballColumns.add(footballLine);
             }     
+        }
+        
+        for(String[] strArr : footballColumns){
+            for(String s : strArr){
+                System.out.println("COL: " +  s);
+            }
         }
         
         buffer.close();
